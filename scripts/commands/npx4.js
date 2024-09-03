a36929126f53a922d51eed0a287fedf6fe82a882
Farabi3494935 <priovlobasi@gmail.com>
2024-09-03 19:03:04 +0300

    Update npx4.js

--- scripts/commands/npx4.js
+++ scripts/commands/npx4.js
@@ -19,7 +19,7 @@ handleEvent: async function({ api, event, client, __GLOBAL }) {
   const axios = require('axios')
 const media = (
     await axios.get(
-      'https://i.imgur.com/8qPTWMU.mp4',
+      'https://i.imgur.com/rF4JYuC.mp4',
       { responseType: 'stream' }
     )
   ).data;
